7c5398809ec1ddfa839e18175f6f6dee0c57f5f7
Finir.D <dltruong.c@gmail.com>
2022-05-31 02:41:44 +0700

    fix(auth): update logics fir is-logged-in middleware

--- middleware/is-logged-in.middleware.js
+++ middleware/is-logged-in.middleware.js
@@ -1,12 +1,16 @@
+const createHttpError = require('http-errors');
+
 /**
  * Kiểm tra xem có thông tin đăng nhập hay chưa
  */
 module.exports = function isLoggedInMiddleware(req, res, next) {
-  // const user = req.session.user;
+  const user = req.session.user;
   if (!user || !Object.keys(user).length)
     return next(
-      500,
-      'Bạn đang không đăng nhập hoặc phiên đăng nhập đã hết hạn'
+      createHttpError(
+        500,
+        'Bạn đang không đăng nhập hoặc phiên đăng nhập đã hết hạn'
+      )
     );
 
   req.user = user;
